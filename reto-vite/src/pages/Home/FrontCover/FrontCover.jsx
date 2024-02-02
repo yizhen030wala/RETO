a016207489b59ea447948152c06279006b3bb1d5
ralph341629@gmail.com <ralph341629@gmail.com>
2024-02-02 17:04:11 +0800

    2/2 1700 PageLinkOn

--- reto-vite/src/pages/Home/FrontCover/FrontCover.jsx
+++ reto-vite/src/pages/Home/FrontCover/FrontCover.jsx
@@ -1,28 +1,35 @@
-import React from 'react';
-import '../CSS/home.css';
-import bg_land_page from '../../../assets/img_hsc/index/LandingPageBackground.svg';
-import HeaderHome from '../HeaderHome/HeaderHome';
+import React from "react";
+import "../CSS/home.css";
+import bg_land_page from "../../../assets/img_hsc/index/LandingPageBackground.svg";
+import HeaderHome from "../HeaderHome/HeaderHome";
+import { Link } from "react-router-dom"; //物件內用到連結跳轉就需要放
 
 const FrontCover = () => {
-    return (
-        <div className="page_left">
-            <HeaderHome />
-            <div className="area_attention">
-                {/* RETO右側欄位暫時放圖片 */}
-                {/* 標題及按鈕 */}
-                <div className="box_attention">
-                    {/* 標題及按鈕使用position */}
-                    <p className="slogan1">可愛又輕便的行程規劃器</p>
-                    <p className="slogan2">讓旅藤代替苦惱的你</p>
-                    {/* <div className="bg_white bar1"></div>
+  return (
+    <div className="page_left">
+      <HeaderHome />
+      <div className="area_attention">
+        {/* RETO右側欄位暫時放圖片 */}
+        {/* 標題及按鈕 */}
+        <div className="box_attention">
+          {/* 標題及按鈕使用position */}
+          <p className="slogan1">可愛又輕便的行程規劃器</p>
+          <p className="slogan2">讓旅藤代替苦惱的你</p>
+          {/* <div className="bg_white bar1"></div>
                             <div className="bg_white bar2"></div> */}
-                    <div className="btn"><a href="./search.html">免費開始</a></div>
-                    <div className="bg"><img src={bg_land_page} alt="" /></div>
-                    {/* 底圖暫為一張大圖包含天空 */}
-                </div>
+          <Link to="/Search">
+            <div className="btn">
+              <a href="#">免費開始</a>
             </div>
+          </Link>
+          <div className="bg">
+            <img src={bg_land_page} alt="" />
+          </div>
+          {/* 底圖暫為一張大圖包含天空 */}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default FrontCover;
--- reto-vite/src/pages/Search/NavBar/NavBar.jsx
+++ reto-vite/src/pages/Search/NavBar/NavBar.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import '../CSS/Search.css';
+import React from "react";
+import "../CSS/Search.css";
+import { Link } from "react-router-dom"; //物件內用到連結跳轉就需要放
+
 const NavBar = () => {
-    const arr_nav = ['開始探索', '我的景點', '行程表'];
-    const arr_nav_link = ['search', 'collections', 'mytravel'];
-    return (
-        <nav className='nav_search'>
-            <ul>
-                {arr_nav.map((nav, index) => (
-                    <li key={index}><a href={`./${arr_nav_link[index]}.html`}>{nav}</a></li>
-                ))}
-            </ul>
-        </nav>
-    )
-}
+  const arr_nav = ["開始探索", "我的景點", "行程表"];
+  const arr_nav_link = ["search", "collections", "mytravel"];
+  const arr_link = ["Search", "Collections", "MyTrip"];
+  return (
+    <nav className="nav_home">
+      <ul>
+        {arr_nav.map((nav, index) => (
+          <Link to={`/${arr_link[index]}`}>
+            <li key={index}>
+              <a href="#">{nav}</a>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 export default NavBar;
