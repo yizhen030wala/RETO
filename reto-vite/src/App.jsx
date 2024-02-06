a9e5feaf8bd115c586d50567072060ece59b2c2c
letmetellatale <69667552+letmetellatale@users.noreply.github.com>
2024-02-06 16:58:46 +0800

    20240206 16:58 修改首頁入徑

--- reto-vite/src/App.jsx
+++ reto-vite/src/App.jsx
@@ -1,31 +1,70 @@
-// App.jsx 所有的link在這裡跳轉
+// // App.jsx 所有的link在這裡跳轉
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './pages/Home';
+// // import Search from './pages/Search'
+// import Login from './pages/Login';
+// import Test from './pages/Test'
+// import MyTrip from './pages/MyTrip'
+// import MyMap from './pages/MyMap'
+// import Schedule from './pages/Schedule'
+
+// import Reto from './pages/Reto'
+
+// import Search from './pages/Search/Search';
+// import Collections from './pages/Collections/Collections';
+
+// //Test Page
+// import Header from './components-common/Header/Header';
+// import Relevant_Card from './pages/Search/Relevant_Card/Relevant_Card';
+// import Set_Journey from './pages/Search/Set_Journey/Set_journey';
+
+
+// const App = () => {
+//   return (
+//     <>
+//       <Routes>
+
+//         <Route path="home" element={<Home />} />
+//         <Route path="/search" element={<Search />} />
+//         <Route path="/" element={<Reto />}>
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/test" element={<Test />} />
+//           <Route path="/mytrip" element={<MyTrip />} />
+//           <Route path="/schedule" element={<Schedule />} />
+//           <Route path="/mymap" element={<MyMap />} />
+//         </Route>
+
+
+//         <Route path="/Collections" element={<Collections />} />
+
+//         <Route path="/Header" element={<Header />} />
+//         <Route path="/Relevant_Card" element={<Relevant_Card />} />
+//         <Route path="/Set_Journey" element={<Set_Journey />} />
+//       </Routes>
+//     </>
+//   );
+// };
+
+// export default App;
+
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
-// import Search from './pages/Search'
-import Login from './pages/Login';
-import Test from './pages/Test'
-import MyTrip from './pages/MyTrip'
-import MyMap from './pages/MyMap'
-import Schedule from './pages/Schedule'
-
-import Reto from './pages/Reto'
-
-import Search from './pages/Search/Search';
-import Collections from './pages/Collections/Collections';
-
-//Test Page
-import Header from './components-common/Header/Header';
-import Relevant_Card from './pages/Search/Relevant_Card/Relevant_Card';
-import Set_Journey from './pages/Search/Set_Journey/Set_journey';
-
+// 其他 import 省略
 
 const App = () => {
   return (
-    <>
-      <Routes>
+    <Router>
+      {/* 使用 <Navigate> 導向到 Home 頁面 */}
+      <Navigate to="/home" replace />
 
-        <Route path="home" element={<Home />} />
+      <Routes>
+        {/* 定義 Home 路由 */}
+        <Route path="/home" element={<Home />} />
+        
+        {/* 定義其他路由 */}
         <Route path="/search" element={<Search />} />
         <Route path="/" element={<Reto />}>
           <Route path="/login" element={<Login />} />
@@ -35,14 +74,13 @@ const App = () => {
           <Route path="/mymap" element={<MyMap />} />
         </Route>
 
-
         <Route path="/Collections" element={<Collections />} />
 
         <Route path="/Header" element={<Header />} />
         <Route path="/Relevant_Card" element={<Relevant_Card />} />
         <Route path="/Set_Journey" element={<Set_Journey />} />
       </Routes>
-    </>
+    </Router>
   );
 };
 
