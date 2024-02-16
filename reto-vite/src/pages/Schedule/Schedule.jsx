0b727917df5776c1e2370279f3031f39f79cce1f
letmetellatale <69667552+letmetellatale@users.noreply.github.com>
2024-02-16 09:43:02 +0800

    0216 09:42 adjust component-DateSwiper RWD

--- reto-vite/src/pages/Schedule/Schedule.jsx
+++ reto-vite/src/pages/Schedule/Schedule.jsx
@@ -16,45 +16,49 @@ const Schedule = () => {
     return (
         <>
             <main className="Schedule_container">
-                {/* 麵包屑導覽列 */}
-                <div className="wrap">
-                    <BreadCrumb />
-                </div>
 
-                {/* 行程表表頭資訊 */}
-                <div className="tripHead">
-                    {/* 行程表名稱+日期 */}
-                    <div className="tripTitle">
-                        {/* 行程表名稱 */}
-                        <div>
-                            <h3 className='h3_title'>旅藤台南行</h3>
+                {/* bar_stick 手機板專用的部分 */}
+                <div className='bar_stick'>
+                    {/* 麵包屑導覽列 */}
+                    <div className="wrap">
+                        <BreadCrumb />
+                    </div>
+
+                    {/* 行程表表頭資訊 */}
+                    <div className="tripHead">
+                        {/* 行程表名稱+日期 */}
+                        <div className="tripTitle">
+                            {/* 行程表名稱 */}
+                            <div>
+                                <h3 className='h3_title'>旅藤台南行</h3>
+                            </div>
+                            {/* 行程表日期 */}
+                            <div>
+                                <h2 className='day_font day_select'>2022/2/23-2/26</h2>
+                            </div>
                         </div>
-                        {/* 行程表日期 */}
-                        <div>
-                            <h2 className='day_font day_select'>2022/2/23-2/26</h2>
+                        {/* 功能按鈕 */}
+                        <div className="tripFunc">
+                            {/* 開始:路線整理的燈箱效果 */}
+                            <div className='LB-container'>
+                                <LightboxLine />
+                            </div>
+                            {/* 結束:路線整理的燈箱效果 */}
+
+
+                            {/* 行李清單 */}
+                            <div className='LB-container'>
+                                <Lightbox />
+                            </div>
+
+                            <Link to='/mymap'><span className="set_icon icon-map "></span></Link>
                         </div>
                     </div>
-                    {/* 功能按鈕 */}
-                    <div className="tripFunc">
-                        {/* 開始:路線整理的燈箱效果 */}
-                        <div className='LB-container'>
-                            <LightboxLine />
-                        </div>
-                        {/* 結束:路線整理的燈箱效果 */}
 
-
-                        {/* 行李清單 */}
-                        <div className='LB-container'>
-                            <Lightbox />
-                        </div>
-
-                        <Link to='/mymap'><span className="set_icon icon-map "></span></Link>
+                    {/* 行程天數列 */}
+                    <div className='dataSwiper_wrap'>
+                        <DateSwiper />
                     </div>
-                </div>
-
-                {/* 行程天數列 */}
-                <div className='dataSwiper_wrap'>
-                <DateSwiper />
                 </div>
 
                 {/* 行程表顯示外框 */}
