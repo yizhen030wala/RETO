f7fb2d2570089ea65e5673ba38a313246a54a65c
Ralph <ralph341629@gmail.com>
2024-02-17 04:15:55 +0800

    2/17 set_journey

--- reto-vite/src/pages/Search/Set_Journey/Set_journey.jsx
+++ reto-vite/src/pages/Search/Set_Journey/Set_journey.jsx
@@ -15,7 +15,28 @@ const Set_Journey = () => {
 
   const switchToPage = (page) => {
     setCurrentPage(page);
+    const setJourneyButton = document.querySelector(".btn_switch_set_journey");
+    const addJourneyButton = document.querySelector(".btn_switch_add_journey");
+    const setButton = document.querySelector(".btn_set.journey.set");
+    const addButton = document.querySelector(".btn_set.journey.add");
+
+    if (page === "set_journey") {
+      setJourneyButton.classList.add("active");
+      addJourneyButton.classList.remove("active");
+      setButton.classList.add("active");
+      addButton.classList.remove("active");
+    } else {
+      setJourneyButton.classList.remove("active");
+      addJourneyButton.classList.add("active");
+      setButton.classList.remove("active");
+      addButton.classList.add("active");
+    }
   };
+
+  // 初始設置為 set_journey
+  useEffect(() => {
+    switchToPage("set_journey");
+  }, []); // 空的依賴列表確保只執行一次
 
   // ↓↓處理編輯名稱部分↓↓
   const [isEditing, setIsEditing] = useState(false);
@@ -78,9 +99,8 @@ const Set_Journey = () => {
           <div className="carousel_set_journey">
             {/* 建立行程頁面 */}
             <div
-              className={`box_switch_set_journey ${
-                currentPage === "set_journey" ? "active" : ""
-              }`}
+              className={`box_switch_set_journey ${currentPage === "set_journey" ? "active" : ""
+                }`}
             >
               <h4 className="title_set_journey">行程設定</h4>
               {/* 可以編輯名稱區域 */}
@@ -118,7 +138,7 @@ const Set_Journey = () => {
                       setDateRange(update);
                     }}
                     placeholderText={`請選擇日期`} // 設置尚未選擇日期時顯示的預設文字
-                    // customInput={<CustomInput />}
+                  // customInput={<CustomInput />}
                   />
                 </div>
               </div>
@@ -128,9 +148,8 @@ const Set_Journey = () => {
             </div>
             {/* 加入行程頁面 */}
             <div
-              className={`box_switch_add_journey ${
-                currentPage === "add_journey" ? "active" : ""
-              }`}
+              className={`box_switch_add_journey ${currentPage === "add_journey" ? "active" : ""
+                }`}
             >
               {/* 這裡要放更新內容 */}
               <div className="content_notyet"></div>
@@ -143,7 +162,8 @@ const Set_Journey = () => {
           <img src={cancel} alt="" />
         </figure>
         {/* 加入按鈕(置於右底) */}
-        <button className="btn_set journey">建立行程</button>
+        <button className="btn_set journey set">建立行程</button>
+        <button className="btn_set journey add">加入行程</button>
       </div>
     </div>
   );
