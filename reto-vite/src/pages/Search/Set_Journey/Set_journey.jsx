5227bd4f0a7d08eb792023763169df3ea8a75185
letmetellatale <69667552+letmetellatale@users.noreply.github.com>
2024-02-21 14:39:27 +0800

    02/21 14:38 /set_journey燈箱效果RWD完成

--- reto-vite/src/pages/Search/Set_Journey/Set_journey.jsx
+++ reto-vite/src/pages/Search/Set_Journey/Set_journey.jsx
@@ -3,7 +3,7 @@ import DatePicker from "react-datepicker";
 import "./Set_Journey.scss";
 import "./datepicker.scss";
 import cancel from "../../../assets/img_hsc/card_viewpoint/cancel.svg";
-import MyTripCard from "../../../components-common/MyTripCard/MyTripCard";
+import JourneyCard from "./JourneyCard.jsx";
 
 const Set_Journey = ({ image, onClose }) => {
 
@@ -168,47 +168,26 @@ const Set_Journey = ({ image, onClose }) => {
             >
               {/* 這裡要放更新內容 */}
               <div className="myTrip_contain_set_journey">
-                {/* 第一個行程集卡片 */}
+                
 
-                <MyTripCard
+                <JourneyCard
                   imageUrl="https://images.unsplash.com/photo-1583889659384-64d9df2347ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   name="台北吃到飽"
                   days="3天2夜"
                   // onNameClick={handleNameClick}
-                ></MyTripCard>
-
-                {/* <MyTripCard
-                  imageUrl="https://images.unsplash.com/photo-1583889659384-64d9df2347ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  name="台北吃到飽"
-                  days="3天2夜"
-                ></MyTripCard> */}
+                ></JourneyCard>
 
                 {/* 第二個行程集卡片 */}
-                <MyTripCard
+                <JourneyCard
                   imageUrl="https://images.unsplash.com/photo-1580745168634-33c78f4c4177?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   name="西門浪流連"
                   days="2天1夜"
                   // onNameClick={handleNameClick}
                 />
 
-                {/* 第三個行程集卡片 */}
-                {/* <MyTripCard
-                  imageUrl="https://images.unsplash.com/photo-1630244024081-dc4039254a46?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  name="宜起蘭去玩"
-                  days="3天2夜"
-                  // onNameClick={handleNameClick}
-                /> */}
-
-                {/* 第四個行程集卡片
-                <MyTripCard
-                  imageUrl="https://images.unsplash.com/photo-1571474004502-c1def214ac6d?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  name="快閃九份"
-                  days="1天0夜"
-                  // onNameClick={handleNameClick}
-                /> */}
 
                 {/* 第五個行程集卡片 */}
-                <MyTripCard
+                <JourneyCard
                   imageUrl="https://images.unsplash.com/photo-1621316489519-77626e5e38fb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   name="台男逛台南"
                   days="3天2夜"
